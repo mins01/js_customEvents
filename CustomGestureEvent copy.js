0e42fe37b1c8417d115ef010658b380c657d08c9
mins01 <mins01.lycos.co.kr@gmail.com>
2023-08-28 14:42:48 +0900

    제스쳐 쪽

--- CustomGestureEvent copy.js
+++ CustomGestureEvent copy.js
@@ -1,8 +1,8 @@
 "use strict";
 /**
- * PointerEvent 등 를 확장해서 custom event를 triger 해서 사용할 수 있게 한다.
+ * 제스쳐 이벤트 에뮬레이팅
  */
-class CustomPointerEvent extends CustomEvent{
+class CustomGestureEvent extends CustomEvent{
     
     static actived = false;
     static target = null; // 최초 이벤트 발생 요소(pointerdown 에서 event.target)
@@ -11,11 +11,21 @@ class CustomPointerEvent extends CustomEvent{
     static cancelable = true; // 이벤트 취소 가능?
     static composed = true; // 셰도루트에서 이벤트가 나갈 수 있는가?
 
+    // longPress
+    static longPressTimeout = 500; //0.5 sec. for Long press
+    static longPressTimeoutTm = null; //
+
+
+
     // 최초 클릭 위치
     static pageX0 = null; // event.pageX;
     static pageY0 = null; // event.pageY;
     static pageX1 = null; // event.pageX;
     static pageY1 = null; // event.pageY;
+
+    // 클릭 시간
+    static timeStamp0 = null;
+    static timeStamp1 = null;
 
     // moveX = null; // pageX1 - pageX0
     // moveY = null; // pageY1 - pageY0
@@ -28,6 +38,11 @@ class CustomPointerEvent extends CustomEvent{
         if(this.pageY1 === null){ return null;}
         if(this.pageY0 === null){ return null;}
         return this.pageY1 - this.pageY0;
+    }
+
+    static get pressedTime(){
+        if(this.timeStamp0 === null){ return null;}
+        return this.timeStamp1 - this.timeStamp0;
     }
 
     //=== 전역 메소드 
@@ -45,12 +60,12 @@ class CustomPointerEvent extends CustomEvent{
         if(!globalThis.window){ throw('window is not exists'); }
         if(this.actived){ console.warn('already actived'); }
         this.actived = true;
-        globalThis.window.addEventListener('pointerdown',this.cbPointerdown);
+        globalThis.window.addEventListener('pointerdown',this.pointerdown);
     }
     static removeEventListener(){
         if(!globalThis.window){ throw('window is not exists'); }
         this.actived = false;
-        globalThis.window.removeEventListener('pointerdown',this.cbPointerdown);
+        globalThis.window.removeEventListener('pointerdown',this.pointerdown);
     }
 
     static options(event){
@@ -66,41 +81,52 @@ class CustomPointerEvent extends CustomEvent{
             target:this.target,
             moveX:this.moveX,
             moveY:this.moveY,
+            pressedTime:this.pressedTime,
             event:event, // original event
         }
     }
-    static cbPointerdown = (event) =>{
-        return this.pointerdown(event)
-    }
-    static pointerdown(event){
+
+    static pointerdown = (event)=>{
         this.target = event.target;
         this.pageX0 = event.pageX;
         this.pageY0 = event.pageY;
         this.pageX1 = event.pageX;
         this.pageY1 = event.pageY;
+        this.timeStamp0 = event.timeStamp;
+        this.timeStamp1 = event.timeStamp;
 
         this.target.dispatchEvent((new this('custompointerdown', this.options(event))));
 
-        document.addEventListener('pointermove',this.cbPointermove);
-        document.addEventListener('pointerup',this.cbPointerup);
+        this.longPressTimeoutTm = setTimeout(() => {
+            this.target.dispatchEvent((new this('longpress', this.options(event))));
+        }, this.longPressTimeout);
+
+        document.addEventListener('pointermove',this.pointermove);
+        document.addEventListener('pointerup',this.pointerup);
     }
-    static cbPointermove = (event) =>{
-        return this.pointermove(event)
-    }
-    static pointermove(event){
+    static pointermove = (event)=>{
         this.pageX1 = event.pageX;
         this.pageY1 = event.pageY;
+        this.timeStamp1 = event.timeStamp;
 
         this.target.dispatchEvent((new this('custompointermove', this.options(event))));
+
+        if(this.longPressTimeoutTm){
+            // console.log('cancel longpress');
+            clearTimeout(this.longPressTimeoutTm); 
+        }
     }
-    static cbPointerup = (event) =>{
-        return this.pointerup(event)
-    }
-    static pointerup(event){
+    static pointerup = (event)=>{
+        this.timeStamp1 = event.timeStamp;
         this.target.dispatchEvent((new this('custompointerup', this.options(event))));
 
-        document.removeEventListener('pointermove',this.cbPointermove);
-        document.removeEventListener('pointerup',this.cbPointerup);
+        if(this.longPressTimeoutTm){
+            // console.log('cancel longpress'); 
+            clearTimeout(this.longPressTimeoutTm); 
+        }
+
+        document.removeEventListener('pointermove',this.pointermove);
+        document.removeEventListener('pointerup',this.pointerup);
     }
 
 
